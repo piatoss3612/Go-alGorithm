bff2e2f41e40047a9abe85dc9d13f3ce41324645
Hyohwak Lee <crewe1746@naver.com>
2023-04-30 16:22:06 +0900

    Solved leetcode 169. Majority Element 2nd solution

--- leetcode/169. Majority Element.go
+++ leetcode/169. Majority Element.go
@@ -16,3 +16,29 @@ func majorityElement(nums []int) int {
 	}
 	return -1
 }
+
+// 2nd solution - Boyer-Moore Voting Algorithm
+// Runtime: 17ms
+// Memory Usage: 6.2 MB
+// Time complexity: O(n)
+// Space complexity: O(1)
+// Ref: https://leetcode.com/problems/majority-element/solutions/3220196/beats-99-87-with-go/
+func majorityElement2(nums []int) int {
+	candidate := nums[0]
+	cnt := 1
+
+	for i := 1; i < len(nums); i++ {
+		if nums[i] == candidate {
+			cnt += 1
+		} else {
+			cnt -= 1
+		}
+
+		if cnt == 0 {
+			candidate = nums[i]
+			cnt = 1
+		}
+	}
+
+	return candidate
+}
